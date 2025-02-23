8899d161db009684d36355bc4cf65cfe5622b874
Nagul75 <nagulk697@gmail.com>
2025-02-23 22:32:55 +0530

    Refactor code to express

--- app.js
+++ app.js
@@ -1,48 +1,24 @@
-const http = require('http')
-const fs = require('fs/promises')
-const url = require('url')
+const express = require('express')
 const path = require('path')
+const app = express()
 
-http.createServer(async (req, res) => {
-    if (req.url == "/") {
-        try {
-            const data = await fs.readFile("index.html")
-            res.writeHead(200, {"Content-Type": "text/html"})
-            res.end(data)
-        }
-        catch(err) {
-            console.log(err)
-        }
-    }
-    else if(req.url == "/about") {
-        try {
-            const data = await fs.readFile(path.join(".", "public", "about.html"))  //path: ./public/about.html
-            res.writeHead(200, {"Content-Type" : "text/html"})
-            res.end(data)
-        }
-        catch(err) {
-            console.log(err)
-        }
-    }
-    else if(req.url == "/contact") {
-        try {
-            const data = await fs.readFile(path.join(".", "public", "contact-me.html"))  //path: ./public/about.html
-            res.writeHead(200, {"Content-Type" : "text/html"})
-            res.end(data)
-        }
-        catch(err) {
-            console.log(err)
-        }
-    }
-    else {
-        try {
-            const data = await fs.readFile(path.join(".", "public", "404-error.html"))  //path: ./public/about.html
-            res.writeHead(200, {"Content-Type" : "text/html"})
-            res.end(data)
-        }
-        catch(err) {
-            console.log(err)
-        }
-    }
-}).listen(8080)
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"))
+})
 
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname,"public", "about.html"))
+})
+
+app.get("/contact-me", (req, res) => {
+    res.sendFile(path.join(__dirname,"public", "contact-me.html"))
+})
+
+app.get('*', function(req, res){
+    res.status(404).send('what???');
+  });
+
+
+app.listen(3001, () => {
+    console.log(`listening on port ${3001}`)
+})
